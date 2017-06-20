4b77017b8fc52c91b662e125bb56314f538f2d55
MYunFeiYang <2728363142@qq.com>
2017-06-20 20:57:18 +0800

    提交

--- main/main.js
+++ main/main.js
@@ -1,6 +1,6 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
-    console.log(`o) 最小值 = ${sequence.minimum()
+    console.log(`o) 最小值 = ${sequence.minimum()}
 	o) 最大值 = ${sequence.maximum()}
 	o) 元素数量 = ${sequence.number()}
 	o) 平均值 = ${sequence.average()}`);
